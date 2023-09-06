d0e2b911081935432445811b74e6f92d1fae1516
katiemayjones <katiemayjones1990@gmail.com>
2023-09-06 10:09:16 +0100

    Added new projects
    Added interactivity

--- src/script.js
+++ src/script.js
@@ -30,3 +30,18 @@ function changeDescription() {
   }, 3000);
 }
 changeDescription();
+
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
